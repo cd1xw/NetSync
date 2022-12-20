f4f25b59427ce5908dfc4d5613b74114822c8636
puremintwater <>
2022-12-20 15:08:35 +1100

    file handler

--- main.go
+++ main.go
@@ -6,18 +6,25 @@ import (
   "bufio"
   "strings"
   "github.com/fatih/color"
+  _ "github.com/lunux2008/xulu"
 )
 func main() {
   if (len(os.Args) == 1){
     color.New(color.FgRed, color.Bold).Printf(fmt.Sprint("\033[31m", "netsync: program-error[cmd-not-specified]: for more commands run `netsync help` \n"))
     return
   }
-  if (os.Args[1] == "open"){
-    if (len(os.Args) == 2){
+  if (os.Args[1] == "open" || len(strings.Split(os.Args[1], ".")) == 2){
+    if (len(os.Args) == 2 && len(strings.Split(os.Args[1], ".")) == 1){
       color.New(color.FgRed, color.Bold).Printf(fmt.Sprint("\033[31m", "netsync: program-error[file-not-specified]: specify a script to run \n"))
       return
     }
-   file, err := os.Open(os.Args[2]) 
+    var thingtoopen string 
+    if len(strings.Split(os.Args[1], ".")) == 2 {
+      thingtoopen = os.Args[1]
+    } else { 
+      thingtoopen = os.Args[2]
+    }
+   file, err := os.Open(thingtoopen) 
 	 if err != nil {
 		fmt.Println(err)
 		return
