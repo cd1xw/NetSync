5f1a8375f969c5e235101c5aa3a853c89586399d
runtime <72543405+RAFhub6@users.noreply.github.com>
2022-12-20 23:37:28 +0000

    throw

--- parser.go
+++ parser.go
@@ -13,6 +13,7 @@ import (
 func scancode(code []string, line int) string {
   var output string
   var serror = color.New(color.FgRed, color.Bold) 
+  var swarning = color.New(color.FgYellow, color.Bold)
   xulu.Use(output)
   if code[0] == "print"{  
     if len(code) > 1 && code[1] != "" {
@@ -26,7 +27,18 @@ func scancode(code []string, line int) string {
   } else if code[0] == "pause"{
     fmt.Println("Press Enter to continue...")
     bufio.NewReader(os.Stdin).ReadBytes('\n') 
-  } else if code[0] == "" {} else {
+  } else if code[0] == "" {} else if code[0] == "throw"{
+    if len(code) > 2 && code[1] != "" && code[2] != "" {
+      if code[1] == "error" {
+        serror.Printf(fmt.Sprint(code[2], "  - line ", line, "\n"))
+      } else if code[1] == "warning" {
+        swarning.Printf(fmt.Sprint(code[2], " - line ", line, "\n"))
+      }
+    } else {
+      output = ""
+      serror.Printf(fmt.Sprint("netsync: syntax-error[argument-not-specified]: throw expected 2 args but got less - line ", line, "\n"))
+    }
+  } else {
     output = ""
     serror.Printf(fmt.Sprint("netsync: syntax-error[not-defined]: the function is not defined - line ", line, "\n"))
   }
