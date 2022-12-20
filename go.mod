e1a5709f9ef415ba4a9f8128e3286c8130ef4314
puremintwater <>
2022-12-20 14:09:04 +1100

    open

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
 	github.com/lunux2008/xulu v0.0.0-20160308154621-fff51ca7218e // indirect
 )
--- main.go
+++ main.go
@@ -9,26 +9,35 @@ import (
 )
 func main() {
   if (len(os.Args) == 1){
-    color.New(color.FgRed, color.Bold).Printf(fmt.Sprint("\033[31m", "netsync: program-error[script-not-specified]: provide a script to run\n"))
+    color.New(color.FgRed, color.Bold).Printf(fmt.Sprint("\033[31m", "netsync: program-error[cmd-not-specified]: for more commands run `netsync help` \n"))
     return
   }
-	file, err := os.Open(os.Args[1]) 
-	if err != nil {
+  if (os.Args[1] == "open"){
+    if (len(os.Args) == 2){
+      color.New(color.FgRed, color.Bold).Printf(fmt.Sprint("\033[31m", "netsync: program-error[file-not-specified]: specify a script to run \n"))
+      return
+    }
+   file, err := os.Open(os.Args[2]) 
+	 if err != nil {
 		fmt.Println(err)
 		return
-	}
-	defer file.Close()
+	 }
+	 defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-  var thing int
-	for scanner.Scan(){
-    thing = thing + 1
-    if (scancode(strings.Split(scanner.Text(), ", "), thing) != ""){
-      fmt.Println(scancode(strings.Split(scanner.Text(), ", "), thing))
-    }
-	}
+	 scanner := bufio.NewScanner(file)
+   var thing int
+	 for scanner.Scan(){
+     thing = thing + 1
+     if (scancode(strings.Split(scanner.Text(), ", "), thing) != ""){
+       fmt.Println(scancode(strings.Split(scanner.Text(), ", "), thing))
+     }
+   }
 
-	if err := scanner.Err(); err != nil {
+	 if err := scanner.Err(); err != nil {
 		fmt.Println(err)
-	}
+	 }
+ } else {
+   color.New(color.FgRed, color.Bold).Printf(fmt.Sprint("\033[31m", "netsync: program-error[invalid-cmd]: for more commands run `netsync help` \n"))
+   return
+ }
 }
